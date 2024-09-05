5ff8b2fd3dc3a6b3b0dd74978451b7ed90b13bd7
Sai Surisetti <sai.surisetti@student.griffith.ie>
2024-09-05 15:17:10 +0100

    Seeds

--- entry.py
+++ entry.py
@@ -8,10 +8,7 @@ seeds = known_seeds('training')
 
 selected_seed = 8501
 
-x = 1
 for s in seeds:
-    if (x == 0):
-        break
     givens = ProblemData()
     actual_demand = InputDemandDataActual(seed = selected_seed)
     inventory = Inventory(givens)
@@ -19,7 +16,6 @@ for s in seeds:
     print(givens)
     print(actual_demand)
     print(inventory)
-    solution_json = app.solution_function(givens, actual_demand, seed = selected_seed, time_steps=time_steps_send, debugging=True)
+    solution_json = app.solution_function(givens, actual_demand, seed = s, time_steps=time_steps_send, debugging=True)
     # SAVE YOUR SOLUTION
-    save_solution(solution_json, f'{outputdirectory}/{s}.json')
-    x -= 1
+    #save_solution(solution_json, f'{outputdirectory}/{s}.json')
